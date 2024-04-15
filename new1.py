a19311a08a67f563950b351bd00ddd61ce2fecdd
Vikaash16 <vikaash3545@gmail.com>
2024-04-15 13:46:49 +0530

    Success

---
+++ new1.py
@@ -0,0 +1,10 @@
+'''Swapping of two valuees 
+Developed by : P.Vikaash
+Register Number : 212223240180'''
+
+Surya_data=int(input())
+Vijay_data=int(input())
+tmp=Surya_data
+Surya_data=Vijay_data
+Vijay_data=tmp
+print("Swapped values are:",Surya_data,Vijay_data)
